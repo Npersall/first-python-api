4f8e50e1f52c18ccac158d9d226fc1e96aa7611e
Nathan Persall <npersall@gmail.com>
2020-03-16 16:39:32 -0700

    Add Employees GET method and SQL Call

--- server.py
+++ server.py
@@ -4,8 +4,13 @@ from sqlalchemy import create_engine
 from json import dumps
 from flask.ext.jsonpify import jsonpify
 
-#connnects database
+
 db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
 
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect() #connect to database
+        query = conn.execute("select * from employees") #SQL Query and returns Json
+        
