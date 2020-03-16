fe6fdd819e44391a9cafe43bdf3bb00af21be9a7
Nathan Persall <npersall@gmail.com>
2020-03-16 16:35:42 -0700

    connect database to app

--- server.py
+++ server.py
@@ -3,3 +3,9 @@ from flask_resful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
 from flask.ext.jsonpify import jsonpify
+
+#connnects database
+db_connect = create_engine('sqlite:///chinook.db')
+app = Flask(__name__)
+api = Api(app)
+
