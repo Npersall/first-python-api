94f883c9b6879523c4103f8e6d04e450578f2878
Nathan Persall <npersall@gmail.com>
2020-03-16 17:06:47 -0700

    create Tracks GET request

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
-from flask_resful import Resource, Api
+from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
-from flask.ext.jsonpify import jsonpify
+from flask_jsonpify import jsonify
 
 
 db_connect = create_engine('sqlite:///chinook.db')
@@ -13,4 +13,11 @@ class Employees(Resource):
     def get(self):
         conn = db_connect.connect() #connect to database
         query = conn.execute("select * from employees") #SQL Query and returns Json
-        
+        return {'employees': [i[0] for i in query.cursor.fetchall() ]} # Fetches the fist column that is Employee ID
+
+class Tracks(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        result = {'data': [dict(zip(tuple(query.keys()) , i)) for i in query.cursor]}
+        return jsonify(result)
